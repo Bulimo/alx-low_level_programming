12c560b2614f7821cf2eaa074c6e561309430ae8
Bulimo <ubaga91@gmail.com>
2023-03-17 23:29:07 +0000

    Correctd the file : 02-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,38 +10,37 @@
 
 int main(void)
 {
-	int i = 48;
+	int i = '0';
+	int j;
+	int k;
+	int l;
 
-	while (i <= 57)
+	while (i <= '9')
 	{
-		int j = 48;
-
-		while (j < 57)
+		j = '0';
+		while (j <= '9')
 		{
-			int k = i;
-
-			while (k <= 57)
+			k = i;
+			l = j + 1;
+			while (k <= '9')
 			{
-				int l;
-
-				if (j == 57)
-					l = 48;
-				else
-					l = j + 1;
-				while (l <= 57)
+				if (l == '0')
+					l = '0';
+				while (l <= '9')
 				{
 					putchar(i);
 					putchar(j);
 					putchar(' ');
 					putchar(k);
 					putchar(l);
-					if (j < 56)
+					if (!((i == '9' && j == '8') && (k == '9' && l == '9')))
 					{
 						putchar(',');
 						putchar(' ');
 					}
 					l++;
 				}
+				l = '0';
 				k++;
 			}
 			j++;
