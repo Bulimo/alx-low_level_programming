5ac2c9e06716b1247dd6b3b27ebf26c1de4bd9df
Bulimo <ubaga91@gmail.com>
2023-03-23 16:23:29 +0000

    Reduced the number of variables used in 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
   * print_number - function that prints an integer
@@ -7,30 +8,30 @@
   */
 void print_number(int n)
 {
-	char neg = '+';			/* check for negative number */
-	int mod = 0;			/* extract the last digit */
 	int rev_n = 0;			/* Store the reversed number */
+	int mod = 0;			/* stores the extracted number */
 
-	if (n < 0)
-	{
-		neg = '-';
-		n *= -1;		/* make the number positive */
-	}
 	if (n == 0)			/* check if number is -ve */
 		_putchar('0' + mod);
-	while (n > 0)
+	else
 	{
-		mod = n % 10;
-		rev_n = (rev_n * 10) + mod;
-		n /= 10;
-	}
-	/* Print out the reversed number */
-	if (neg == '-')
-		_putchar('-');
-	while (rev_n > 0)
-	{
-		mod = rev_n % 10;
-		rev_n /= 10;
-		_putchar('0' + mod);
+		if (n < 0)
+		{
+			n *= -1;
+			_putchar('-');
+		}
+		while (n > 0)
+		{
+			mod = n % 10;
+			rev_n = (rev_n * 10) + mod;
+			n /= 10;
+		}
+		/* Print out the reversed number */
+		while (rev_n > 0)
+		{
+			mod = rev_n % 10;
+			rev_n /= 10;
+			_putchar('0' + mod);
+		}
 	}
 }
