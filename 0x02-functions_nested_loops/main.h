9e57dd254072ebee51b83cf87dcc33a4a8c05ea3
Bulimo <ubaga91@gmail.com>
2023-03-23 21:48:19 +0000

    Added all the prototypes in main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,26 +3,30 @@
 #include <unistd.h>
 #include <stdio.h>
 
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
 int _putchar(char c);
 
-/**
-  * times_table - prints the 9 times table starting with 0
-  * Return: void
-  */
 void times_table(void);
 
-/**
-  * print_times_table - priints the n times table starting with 0
-  *	if n > 15 or n < 0 the function should not print
-  * @n: the size of the times table
-  * Return: void
-  */
 void print_times_table(int n);
+
+void print_alphabet(void);
+
+void print_alphabet_x10(void);
+
+int _islower(int c);
+
+int _isalpha(int c);
+
+int print_sign(int n);
+
+int _abs(int n);
+
+int print_last_digit(int n);
+
+void jack_bauer(void);
+
+int add(int, int);
+
+void print_to_98(int n);
+
 #endif
