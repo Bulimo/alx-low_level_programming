4e39e5a4f60753939b0763b1c84640c813b139c0
Bulimo <ubaga91@gmail.com>
2023-03-23 14:52:12 +0000

    Made changes on 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
   * print_number - function that prints an integer
@@ -11,29 +12,26 @@ void print_number(int n)
 	int mod = 0;			/* extract the last digit */
 	int rev_n = 0;			/* Store the reversed number */
 
-	if (n == 0)
-		_putchar('0');
-	else
+	if (n < 0)
 	{
-		if (n < 0)			/* check if number is -ve */
-		{
-			neg = '-';
-			n *= -1;		/* make the number positive */
-		}
-		while (n > 0)
-		{
-			mod = n % 10;
-			rev_n = (rev_n * 10) + mod;
-			n /= 10;
-		}
-		/* Print out the reversed number */
-		if (neg == '-')
-			_putchar('-');
-		while (rev_n > 0)
-		{
-			mod = rev_n % 10;
-			rev_n /= 10;
-			_putchar('0' + mod);
-		}
+		neg = '-';
+		n *= -1;		/* make the number positive */
+	}
+	if (n == 0)			/* check if number is -ve */
+		_putchar('0' + mod);
+	while (n > 0)
+	{
+		mod = n % 10;
+		rev_n = (rev_n * 10) + mod;
+		n /= 10;
+	}
+	/* Print out the reversed number */
+	if (neg == '-')
+		_putchar('-');
+	while (rev_n > 0)
+	{
+		mod = rev_n % 10;
+		rev_n /= 10;
+		_putchar('0' + mod);
 	}
 }
