5c7bcb11804b303d52ec5ef97905433ff6fea076
Bulimo <ubaga91@gmail.com>
2023-03-24 18:27:29 +0000

    updated 5-more_numbers.c function to a better implementation

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,23 +8,14 @@ void more_numbers(void)
 {
 	int i = 0;
 	int j = 0;
-	int k = 0;
-	int l = 0;
 
 	for (; i < 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			if (j <= 9)
-				k = j;
-			else
-			{
-				k = 1;
-				l = j % 10;
-			}
-			_putchar('0' + k);
 			if (j > 9)
-				_putchar('0' + l);
+				_putchar('0' + (j / 10));
+			_putchar('0' + (j % 10));
 		}
 		_putchar('\n');
 	}
