12ceefbc587a3d8dfb3fa830db20578d2c6d7a76
Bulimo <ubaga91@gmail.com>
2023-03-30 17:44:11 +0000

    Modified file 102-magic.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,19 +7,20 @@
   */
 void print_number(int n)
 {
-	int num = 0;			/* holds the tens multiples of n */
-	int quotient = 0;		/* holds the division of n by num */
-	double size = 1.0;		/* to get the size of largest int */
+	unsigned int num = 0;			/* holds the tens multiples of n */
+	unsigned int quotient = 0;		/* holds the division of n by num */
+	unsigned int n1 = n;			/* holds a copy of n */
+	double size = 1.0;				/* to get the size of largest int */
 
 	/* check for -ve number */
 	if (n < 0)
 	{
-		n *= -1;
+		n1 *= -1;
 		_putchar('-');
 	}
 
 	/* Get the size of the int */
-	while (size <= n)
+	while (size <= n1)
 		size *= 10;
 	num = size / 10;
 
@@ -28,9 +29,9 @@ void print_number(int n)
 	{
 		while (num >= 1)
 		{
-			quotient = n / num;
+			quotient = n1 / num;
 			_putchar('0' + quotient);
-			n -= (quotient * num);
+			n1 -= (quotient * num);
 			num /= 10;
 		}
 	}
