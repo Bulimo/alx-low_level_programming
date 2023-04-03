89d98cdc988b280fabf72f1a6671d4d649591668
Bulimo <ubaga91@gmail.com>
2023-04-03 19:33:16 +0000

    Added file 5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+  * char *_strstr -  function that finds 1st instance of a substring
+  * @haystack: string to be searched in
+  * @needle: string to search in haystack
+  * Return: the pointer to beginning of instance or NULL
+  */
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int i = 0;					/* haystack loop counter */
+	unsigned int j = 0;					/* needle loop counter */
+	unsigned int len1 = 0;				/* haystack length */
+	unsigned int len2 = 0;				/* needle llength */
+	unsigned int k = 0;					/* count from start of instance */
+
+	len1 = _strlen(haystack);
+	len2 = _strlen(needle);
+	for (i = 0; i < len1; i++)
+	{
+		if (haystack[i] == needle[j])
+		{
+			k = i;
+			for (j = 0; j < len2; j++, k++)
+			{
+				if (needle[j] != haystack[k])
+				{
+					break;
+				}
+			}
+			if (j == len2 && i < len1)
+				return (&haystack[k - j]);
+		}
+		j = 0;
+	}
+	return (NULL);
+}
+
+/**
+  * _strlen - returns the length of the string passed
+  * @s: the sting to find the length
+  * Return: int length of string
+  */
+int _strlen(char *s)
+{
+	int len = 0;			/* store string length */
+	int i = 0;				/* loop counter */
+
+	while (*(s + i++))
+		len++;
+	return (len);
+}
