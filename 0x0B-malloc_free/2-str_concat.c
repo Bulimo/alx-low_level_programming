707ba104114dc3168873dc4afe6aac06d5209fd7
Bulimo <ubaga91@gmail.com>
2023-04-11 12:43:16 +0000

    Corrected output for NULL case in 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,18 +14,18 @@ char *str_concat(char *s1, char *s2)
 	int j = 0;				/* s2 loop coounter */
 	char *str = NULL;		/* concatenated string */
 
-	if (!(s1 && s2))
-		return (NULL);
-
 	len1 = _strlen(s1);
 	len2 = _strlen(s2);
 	str = malloc(sizeof(char) * (len1 + len2 + 1));
 	if (str == NULL)
 		return (NULL);
-	for (i = 0; i < len1; i++)
-		str[i] = s1[i];
-	for (j = 0; j < len2; j++, i++)
-		str[i] = s2[j];
+	if ((len1 + len2) > 0)
+	{
+		for (i = 0; i < len1; i++)
+			str[i] = s1[i];
+		for (j = 0; j < len2; j++, i++)
+			str[i] = s2[j];
+	}
 	str[i] = '\0';
 	return (str);
 }
@@ -40,7 +40,11 @@ int _strlen(char *s)
 	int len = 0;			/* store string length */
 	int i = 0;				/* loop counter */
 
-	while (*(s + i++))
-		len++;
+	if (s != NULL)
+	{
+
+		while (*(s + i++))
+			len++;
+	}
 	return (len);
 }
