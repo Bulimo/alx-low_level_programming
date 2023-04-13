82a9b4b30a3259b6bea17beb01a2fb5450631bdb
Bulimo <ubaga91@gmail.com>
2023-04-13 18:49:11 +0000

    Added file 100-realloc.c and updated main.h

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+  * _realloc - reallocates memory block using malloc and free
+  * @ptr: the pointer to memory to be reallocated
+  * @old_size: old memory size
+  * @new_size: new memory size
+  * Return: pointer to new memory size, NULL otherwise
+  */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *new_ptr = NULL;			/* pointer to the new memory size */
+	unsigned int i = 0;				/* loop counter */
+
+	if (new_size == old_size)
+		return (ptr);
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (ptr == NULL)
+	{
+		new_ptr = malloc(new_size);
+		if (new_ptr == NULL)
+			return (NULL);
+		return (new_ptr);
+	}
+	new_ptr = malloc(new_size);
+	if (new_ptr == NULL)
+		return (NULL);
+	if (new_size > old_size)
+	{
+		for (i = 0; i < old_size; i++)
+			new_ptr[i] = *((char *)ptr + i);
+		free(ptr);
+		ptr = NULL;
+		return (new_ptr);
+	}
+	if (new_size < old_size)
+	{
+		for (i = 0; i < new_size; i++)
+			new_ptr[i] = *((char *)ptr + i);
+		free(ptr);
+		ptr = NULL;
+		return (new_ptr);
+	}
+	return (NULL);
+}
--- 0x0C-more_malloc_free/README.md
+++ 0x0C-more_malloc_free/README.md
@@ -22,4 +22,5 @@
 	2. 1-string_nconcat.c : a function that concatenates two strings.
 	3. 2-calloc.c : a function that allocates memory for an array, using malloc.
 	4. 3-array_range.c : a function that creates an array of integers.
-	5. 
+	5. 100-realloc.c : a function that reallocates a memory block using malloc and free
+	6. 
