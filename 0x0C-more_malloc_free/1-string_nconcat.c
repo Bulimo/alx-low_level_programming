15170a26373fa0b134f0d7f2c2b7f7904b9ee092
Bulimo <ubaga91@gmail.com>
2023-04-13 13:25:25 +0000

    Corrected file 1-string_nconcat.c output for -ve byte size to copy

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -9,37 +9,28 @@
   */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *str;								/* pointer to new concatenated string */
-	unsigned int len1 = 0, len2 = 0;		/* length of s1 & s2 */
-	unsigned int i = 0, j = 0;				/* loop counter */
+	char *str;						/* pointer to new concatenated string */
+	int len1 = 0, len2 = 0;			/* length of s1 & s2 */
+	int i = 0, j = 0;				/* loop counter */
+	int size = n;
 
 	len1 = _strlen(s1);
 	len2 = _strlen(s2);
 
+	if (size < 0)
+		return (NULL);
+	if (size >= len2)
+		size = len2;
 	if (!len1 && !len2)
 		str = malloc(sizeof(char) * 1);
-	else if (len1 && len2)
-	{
-		if (n >= len2)
-			str = malloc(sizeof(char) * (len1 + len2 + 1));
-		else
-			str = malloc(sizeof(char) * (len1 + n + 1));
-	}
-	else if (!len1 && len2)
-	{
-		if (n >= len2)
-			str = malloc(sizeof(char) * (len2 + 1));
-		else
-			str = malloc(sizeof(char) * (n + 1));
-	}
 	else
-		str = malloc(sizeof(char) * (len1 + 1));
+		str = malloc(sizeof(char) * (len1 + size + 1));
 
 	if (str == NULL)
 		return (NULL);
 	for (i = 0; i < len1; i++)
 		str[i] = s1[i];
-	for (j = 0; j < n && s2[j] != '\0'; j++, i++)
+	for (j = 0; j < size; j++, i++)
 		str[i] = s2[j];
 	str[i] = '\0';
 	return (str);
