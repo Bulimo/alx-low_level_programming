1949bd78b8cdefc68c6a76d4ae1e8e2b1d07e772
Bulimo <ubaga91@gmail.com>
2023-04-17 19:33:28 +0000

    corrected the file 4-new_dog.c to allow for memory allocation

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,9 +14,72 @@ dog_t *new_dog(char *name, float age, char *owner)
 	a_dog = malloc(sizeof(struct dog));
 	if (a_dog == NULL)
 		return (NULL);
-	a_dog->name = (name == NULL) ? NULL : name;
+
+	if (name == NULL)
+		a_dog->name = NULL;
+	else
+	{
+		a_dog->name = malloc(sizeof(char) * (_strlen(name) + 1));
+		if (a_dog->name == NULL)
+		{
+			free(a_dog);
+			return (NULL);
+		}
+		_strcpy(a_dog->name, name);
+	}
+
+	if (owner == NULL)
+		a_dog->owner = NULL;
+	else
+	{
+		a_dog->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+		if (a_dog->owner == NULL)
+		{
+			free(a_dog);
+			return (NULL);
+		}
+		_strcpy(a_dog->owner, owner);
+	}
 	a_dog->age = age;
-	a_dog->owner = (owner == NULL) ? NULL : owner;
 
 	return (a_dog);
 }
+
+/**
+  * _strlen - returns the length of the string passed
+  * @s: the sting to find the length
+  * Return: int length of string
+  */
+int _strlen(char *s)
+{
+	int len = 0;			/* store string length */
+	int i = 0;				/* loop counter */
+
+	if (s != NULL)
+	{
+		while (*(s + i++))
+			len++;
+	}
+	return (len);
+}
+
+/**
+  * _strcpy - copies the string src to dest
+  * @src: source string to be copied
+  * @dest: where to copy src
+  * Return: void
+  */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;			/* loop counter */
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
+
