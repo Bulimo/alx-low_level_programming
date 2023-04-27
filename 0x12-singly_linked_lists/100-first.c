78cfa244f04210588010923a7d7ce77d86e4d483
Bulimo <ubaga91@gmail.com>
2023-04-27 16:14:52 +0000

    betty corrections on file 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,6 +7,6 @@
   */
 void first(void)
 {
-	printf("%s", "You're beat! and yet, you must allow"
-				",\nI bore my house upon my back!\n");
+	printf("%s", "You're beat! and yet, you must allow,");
+	printf("%s", "\nI bore my house upon my back!\n");
 }
