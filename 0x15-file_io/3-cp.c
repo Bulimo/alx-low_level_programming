4abc1cf15c27e807fd4e9c6c252d07de7ef5b769
Bulimo <ubaga91@gmail.com>
2023-05-09 00:48:16 +0000

    added file 3-cp.c

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,98 @@
+#include "main.h"
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#define BUFFER  1024
+
+/**
+  * main - start point of the program
+  * @argc: number of arguments passed
+  * @argv: array of arguments passed as strings
+  * Description: program that copies content of a file to another
+  * Return: 0 on Success, 1 otherwise
+  */
+int main(int argc, char **argv)
+{
+	char	buffer[BUFFER] = {'\0'};
+	/*size_t	count = 0	len = 0;*/
+	ssize_t	read_count = 0,	write_count = 0;
+	int		fd_from = 0,	fd_to = 0;
+
+	if (argc != 3)
+		print_error(97, argv[0]);
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+		print_error(98, argv[1]);
+	read_count = read(fd_from, buffer, BUFFER);
+	if (read_count == -1)
+		print_error(98, argv[1]);
+	printf("read_count = %ld\n", read_count);
+	fd_to = open(argv[2], O_CREAT | O_RDWR |
+					O_APPEND | O_TRUNC, 0664);
+	if (fd_to == -1)
+		print_error(99, argv[2]);
+	while (read_count)
+	{
+		write_count = write(fd_to, buffer, read_count);
+		if (write_count == -1 && write_count != read_count)
+			print_error(99, argv[2]);
+		read_count = read(fd_from, buffer, BUFFER);
+		if (read_count == -1)
+			print_error(98, argv[1]);
+		printf("read_count = %ld\n", read_count);
+	}
+	if (close(fd_to) == -1)
+		print_error(fd_to, argv[2]);
+	if (close(fd_from) == -1)
+		print_error(fd_from, argv[1]);
+	return (0);
+}
+
+/**
+  * _strlen - returns the length of the string passed
+  * @s: the sting to find the length
+  * Return: int length of string
+  */
+size_t _strlen(char *s)
+{
+	size_t len = 0;			/* store string length */
+	size_t i = 0;				/* loop counter */
+
+	if (s != NULL)
+	{
+
+		while (*(s + i++))
+			len++;
+	}
+	return (len);
+}
+
+/**
+  * print_error - prints an error
+  * @error: code of the error
+  * @filename: file being accessed
+  * Return: nothing
+  */
+void print_error(int error, char *filename)
+{
+	switch (error)
+	{
+		case 98:
+			dprintf(2, "Error: Can't read from file %s\n", filename);
+			exit(error);
+			break;
+		case 99:
+			dprintf(2, "Error: Can't write to %s\n", filename);
+			exit(error);
+			break;
+		case 97:
+			dprintf(2, "Usage: %s file_from file_to\n", filename);
+			exit(97);
+			break;
+		default:
+			dprintf(2, "Error: Can't close fd %d\n", error);
+			exit(100);
+			break;
+	}
+}
--- 0x15-file_io/README.md
+++ 0x15-file_io/README.md
@@ -26,5 +26,6 @@ i	. What are file descriptors; the 3 standard file descriptors, what are their p
 	1. 0-read_textfile.c :  a function that reads a text file and prints it to the POSIX standard output.
 	2. 1-create_file.c : a function that creates a file.
 	3. 2-append_text_to_file.c :  a function that appends text at the end of a file.
-	4. 
+	4. 3-cp.c : a program that copies the content of a file to another file.
+	5. 
 
